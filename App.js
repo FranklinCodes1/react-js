34ccdfd59d6a9af37877df7366a286ef343726e6
Kevin Franklin <kevinfranklin59@gmail.com>
2021-07-27 13:36:06 -0400

    gr8

--- App.js
+++ App.js
@@ -49,7 +49,7 @@ function App() {
     <input ref={todoNameRef}type="text" />
     <button onClick={handleAddTodo}>Add Todo</button>
     <button onClick={handleClearTodos}>Clear Completed Todos</button>
-    <div>{todos.filter(todo => !todo.complete).length} left to do</div>
+    <div>{todos.filter(todo => !todo.complete).length} left to do!</div>
     </>
     
     )
